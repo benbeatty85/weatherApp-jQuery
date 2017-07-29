c75fefd9ebc6c35eba318c6ebc983e057860da11
Ben Beatty <benbeatty85@gmail.com>
2017-07-29 13:33:10 +0000

    jQuery has been added and is working

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ function getCoordinatesForCity(cityName) {
   var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
 
   return (
-    fetch(url) // Returns a promise for a Response
-    .then(response => response.json()) // Returns a promise for the parsed JSON
+    $.getJSON(url) // Returns a promise for the parsed JSON
     .then(data => data.results[0].geometry.location) // Transform the response to only take what we need
   );
 }
@@ -23,28 +22,27 @@ function getCurrentWeather(coords) {
   var url = `${CORS_PROXY}${DARKSKY_API_URL}${DARKSKY_API_KEY}/${coords.lat},${coords.lng}?units=si&exclude=minutely,hourly,daily,alerts,flags`;
 
   return (
-    fetch(url)
-    .then(response => response.json())
+    $.getJSON(url)
     .then(data => data.currently)
   );
 }
 
 
-var app = document.querySelector('#app');
-var cityForm = app.querySelector('.city-form');
-var cityInput = cityForm.querySelector('.city-input');
-var cityWeather = app.querySelector('.city-weather');
+var app = $('#app');
+var cityForm = app.find('.city-form');
+var cityInput = cityForm.find('.city-input');
+var cityWeather = app.find('.city-weather');
 
-cityForm.addEventListener('submit', function(event) { // this line changes
+cityForm.on('submit', function(event) { // this line changes
   event.preventDefault(); // prevent the form from submitting
 
   // This code doesn't change!
-  var city = cityInput.value;
+  var city = cityInput.val();
 
   getCoordinatesForCity(city)
   .then(getCurrentWeather)
   .then(function(weather) {
-    cityWeather.innerText = "The current temperature is " + weather.temperature + "." + "\n" + " The windspeed is " + weather.windSpeed + " and the visibility is " + weather.visibility + "." + "\n" + "Go outside at your own risk Muah-ha ha ha!";
+    cityWeather.text("The current temperature is " + weather.temperature + "." + "\n" + " The windspeed is " + weather.windSpeed + " and the visibility is " + weather.visibility + "." + "\n" + "Go outside at your own risk Muah-ha ha ha!");
   });
 });
 
